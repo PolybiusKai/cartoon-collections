8880a022e6659380a6411ee526dfdf1b7c4d94a7
rsmith <legendary.belief@gmail.com>
2021-03-03 19:20:37 -0800

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(name)# code an argument here
   # Your code here
+  if name.length === 0
+    puts "No Dwarves Here!"
+  else 
+      name.each_with_index {|name, index| puts " #{index.to_i + 1}. #{name}"}
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_call)# code an argument here
   # Your code here
+  planeteer_call.map {|call| call.capitalize << "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(yum)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  yum.find {|cheese| cheese === "cheddar" || cheese === "gouda" || cheese === "camembert"}
 end
